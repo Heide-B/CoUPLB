c8ab233e3a680ff0a9423883f29173cd1a441c4d
Heide Balcera <77626580+Heide-B@users.noreply.github.com>
2021-11-15 21:49:46 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"heidemlbalcera@gmail.com\"\n\
+email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
@@ -12,3 +12,4 @@ enableCORS=false\n\
 port = $PORT\n\
 [theme]\n\
 font=\"sans serif\"\n
+" > ~/.streamlit/config.toml
