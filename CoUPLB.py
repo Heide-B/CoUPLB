abd9b57cdba8e7a35d6c4bb5ba6471d6b8064d67
Heide Balcera <77626580+Heide-B@users.noreply.github.com>
2021-11-27 22:09:26 +0800

    Update CoUPLB.py

--- CoUPLB.py
+++ CoUPLB.py
@@ -134,7 +134,7 @@ def record(date,clowder,name,present,injure,remarks,feeder):
         'feeder':feeder}
     conn = init_connection()
     with conn.cursor() as cur:
-        cur.execute("""INSERT INTO public.record_1("Timestamp",clowder,"Name",attendance,status,feeder,remarks) VALUES (%(timestamp)s, $(clowder)s, %(name)s, %(attendance)s, %(status)s, %(feeder)s, %(remarks)s)""", records)
+        cur.execute("""INSERT INTO public.record_1("Timestamp",clowder,"Name",attendance,status,feeder,remarks) VALUES (%(timestamp)s, %(clowder)s, %(name)s, %(attendance)s, %(status)s, %(feeder)s, %(remarks)s)""", records)
         conn.commit()
         conn.close()
 
